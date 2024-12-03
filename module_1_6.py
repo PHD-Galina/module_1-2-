acbbca6de4377ede4e510d4532b6e0bbb90f1bbc
PHD-Galina <dr.galinka.1@gmail.com>
2024-12-03 13:55:16 +0300

    Add files via upload

---
+++ module_1_6.py
@@ -0,0 +1,5 @@
+completed_homework = 12
+hours_count = 1.5
+course_name = 'Python'
+middle_time = 1.5 / 12
+print('Курс', course_name,',','всего задач',':', completed_homework,',', 'затрачено часов',':', hours_count,',', 'среднее время выполнения', middle_time, 'часа')
